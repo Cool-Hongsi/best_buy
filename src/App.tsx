7ed0c4aba36485a9009e5d979470c45130423467
Jake Hong <hongsi140626@gmail.com>
2022-09-03 16:32:54 -0400

    testing husky

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import environment from 'environment';
 
 const App = () => {
-  const data = 'abcd';
-
   return (
     <>
       <div data-testid="app-container">{environment.baseUrl}</div>
